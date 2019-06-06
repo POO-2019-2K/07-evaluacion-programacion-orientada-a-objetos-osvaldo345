8eab0c7c8ef14dbb66ed20627ec2bbe754d0addb
osvaldo345 <42743246+osvaldo345@users.noreply.github.com>
2019-06-05 22:35:11 -0500

    agregar botones

--- evaluacion 4/Tarea.js
+++ evaluacion 4/Tarea.js
@@ -20,14 +20,87 @@ _initTables2() {
     });
 }
 
+_cancelEdit(row, tareas) {
+    row.cells[0].innerHTML = tareas.num;
+    row.cells[1].innerHTML = tareas.tarea;
+    row.cells[2].innerHTML =tareas.getFinalAsString();
+    row.cells[3].innerHTML = tareas.getAge();
+    this._addEditDeleteToRow(row, tareas);
+  }
+
+  _saveEdit(row, tareas, newRegistros){
+    let pos = this._findId(tareas.num);
+    this._actividadesr[pos] = newRegistros;
+    localStorage.setItem('taller', JSON.stringify(this._actividades));
+    this._cancelEdit(row, new Registros(newRegistros));
+  }
+
+  _editRow(row, tareas) {
+    let iNum = document.createElement('input');
+    iNum.type = 'text';
+    iNum.value = tareas.num;
+
+    let iTarea = document.createElement('input');
+    iTarea.type = 'text';
+    iTarea.value = tareas.tarea;
+
+    let iDate = document.createElement('input');
+    iDate.type = 'date';
+    iDate.value = tareas.getFinalAsString();
+
+    let btnSave = document.createElement('input');
+    btnSave.type = 'button';
+    btnSave.value = 'Grabar';
+    btnSave.className = "btn btn-success";
+    btnSave.addEventListener('click', () => {
+      let newRegistros = {
+        num: iNum.value,
+        tarea: iTarea.value,
+        final: iDate.value,
+       
+      };
+
+      this._saveEdit(row, tareas, newRegistros);
+    });
+
+    let btnCancel = document.createElement('input');
+    btnCancel.type = 'button';
+    btnCancel.value = 'Cancelar';
+    btnCancel.className = "btn btn-danger";
+    btnCancel.addEventListener('click', () => {
+      this._cancelEdit(row, tareas);
+    });
+
+    row.cells[0].innerHTML = '';
+    row.cells[0].appendChild(iNum);
+    row.cells[1].innerHTML = '';
+    row.cells[1].appendChild(iTarea);
+    row.cells[2].innerHTML = '';
+    row.cells[2].appendChild(iDate);
+    row.cells[4].innerHTML = '';
+    row.cells[4].appendChild(btnSave);
+    row.cells[5].innerHTML = '';
+    row.cells[5].appendChild(btnCancel);
+  }
+
 _addEditDeleteToRow(row, tareas) {
+    let btnEdit = document.createElement("input");
+    btnEdit.type = "button";
+    btnEdit.value = 'Editar';
+    btnEdit.className = 'btn btn-success';
+    btnEdit.addEventListener('click', () => {
+      this._editRow(row, tareas);
+    });
+
     let btnDelete = document.createElement("input");
     btnDelete.type = "button";
     btnDelete.value = 'Eliminar';
     btnDelete.className = 'btn btn-danger';
 
     row.cells[4].innerHTML = '';
-    row.cells[4].appendChild(btnDelete);
+    row.cells[4].appendChild(btnEdit);
+    row.cells[5].innerHTML = '';
+    row.cells[5].appendChild(btnDelete);
     btnDelete.addEventListener('click', () => {
         this._actividades.splice(tareas, 1);
         row.innerHTML = "";
@@ -45,6 +118,7 @@ _addEditDeleteToRow(row, tareas) {
         let cellfinal = row.insertCell(2);
         let cellDif = row.insertCell(3);
         row.insertCell(4);
+        row.insertCell(5);
 
 
         cellnum.innerHTML = tareas.num;
@@ -58,7 +132,6 @@ _addEditDeleteToRow(row, tareas) {
             num: tareas.num,
             tarea: tareas.tarea,
             final: tareas.final,
-            correo: tareas.correo,
         }
             this._actividades.push(objTareas);
     }
